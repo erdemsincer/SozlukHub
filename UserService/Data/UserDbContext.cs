2caed1a76457133aecb8504be15d4db76a834a05
Erdem <erdemsincer123@icloud.com>
2025-05-05 01:20:08 +0300

    User entiy

---
+++ UserService/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using UserService.Entities;
+
+namespace UserService.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ UserService/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserService.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }  // AuthService'ten gelen ID
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
