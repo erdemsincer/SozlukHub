cb935c2d9412c850b6f5efe822b9fcb52114981f
Erdem <erdemsincer123@icloud.com>
2025-05-05 01:33:46 +0300

    user repository

---
+++ UserService/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserService.Entities;
+
+namespace UserService.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByUsernameAsync(string username);
+        Task AddAsync(User user);
+        Task SaveChangesAsync();
+    }
+}
---
+++ UserService/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.Entities;
+
+namespace UserService.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserDbContext _context;
+
+        public UserRepository(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+            => await _context.Users.FindAsync(id);
+
+        public async Task<User?> GetByUsernameAsync(string username)
+            => await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+        public async Task AddAsync(User user)
+            => await _context.Users.AddAsync(user);
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+
+}
