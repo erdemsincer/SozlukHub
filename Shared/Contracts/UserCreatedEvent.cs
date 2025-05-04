78bee2b055b66d2ee20a368cbd3c56bbe01cc2e0
Erdem <erdemsincer123@icloud.com>
2025-05-05 01:35:15 +0300

    event işlemi

---
+++ Shared/Contracts/UserCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Contracts
+{
+    public class UserCreatedEvent
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
