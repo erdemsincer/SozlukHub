4652e4f0e967fd7010e92a15ad82c17516738d2b
Erdem <erdemsincer123@icloud.com>
2025-05-05 01:35:01 +0300

    consumers

---
+++ UserService/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Shared.Contracts;
+using UserService.Entities;
+using UserService.Services;
+
+namespace UserService.Consumers
+{
+    public class UserCreatedConsumer : IConsumer<UserCreatedEvent>
+    {
+        private readonly IUserService _userService;
+
+        public UserCreatedConsumer(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task Consume(ConsumeContext<UserCreatedEvent> context)
+        {
+            var e = context.Message;
+
+            var user = new User
+            {
+                Id = e.Id,
+                Username = e.Username,
+                Email = e.Email
+            };
+
+            await _userService.CreateUserAsync(user);
+        }
+    }
+}
