8cd5635d05de3dd16a10acf3d0ef0ffd26cc35b7
Erdem <erdemsincer123@icloud.com>
2025-05-05 01:33:54 +0300

    user services

---
+++ UserService/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using UserService.Entities;
+
+namespace UserService.Services
+{
+    public interface IUserService
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task CreateUserAsync(User user);
+    }
+}
---
+++ UserService/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using UserService.Entities;
+using UserService.Repositories;
+
+namespace UserService.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repo;
+
+        public UserService(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+            => await _repo.GetByIdAsync(id);
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _repo.AddAsync(user);
+            await _repo.SaveChangesAsync();
+        }
+    }
+}
