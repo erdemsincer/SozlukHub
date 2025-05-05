244ed3f7e37c91cdd4cb66ef90cb06d023cb4b88
Erdem <erdemsincer123@icloud.com>
2025-05-06 00:47:58 +0300

    vote repositories

---
+++ VoteService/Repositories/IVoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using VoteService.Entities;
+
+namespace VoteService.Repositories
+{
+    public interface IVoteRepository
+    {
+        Task<Vote?> GetVoteAsync(int entryId, int userId);
+        Task<List<Vote>> GetVotesByEntryIdAsync(int entryId);
+        Task AddVoteAsync(Vote vote);
+        Task DeleteVoteAsync(Vote vote);
+        Task SaveChangesAsync();
+    }
+}
---
+++ VoteService/Repositories/VoteRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VoteService.Data;
+using VoteService.Entities;
+
+namespace VoteService.Repositories
+{
+    public class VoteRepository : IVoteRepository
+    {
+        private readonly VoteDbContext _context;
+
+        public VoteRepository(VoteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Vote?> GetVoteAsync(int entryId, int userId)
+        {
+            return await _context.Votes
+                .FirstOrDefaultAsync(v => v.EntryId == entryId && v.UserId == userId);
+        }
+
+        public async Task<List<Vote>> GetVotesByEntryIdAsync(int entryId)
+        {
+            return await _context.Votes
+                .Where(v => v.EntryId == entryId)
+                .ToListAsync();
+        }
+
+        public async Task AddVoteAsync(Vote vote)
+        {
+            await _context.Votes.AddAsync(vote);
+        }
+
+        public async Task DeleteVoteAsync(Vote vote)
+        {
+            _context.Votes.Remove(vote);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
