b3621f6604f09ee67eb492effcce761c3983b5b6
Erdem <erdemsincer123@icloud.com>
2025-05-05 22:39:26 +0300

    entry services

---
+++ EntryService/Services/EntryService.cs
@@ -0,0 +1,31 @@
+﻿using EntryService.Entities;
+using EntryService.Repositories;
+
+namespace EntryService.Services
+{
+    public class EntryService : IEntryService
+    {
+        private readonly IEntryRepository _entryRepository;
+
+        public EntryService(IEntryRepository entryRepository)
+        {
+            _entryRepository = entryRepository;
+        }
+
+        public async Task<Entry?> GetByIdAsync(int id)
+        {
+            return await _entryRepository.GetByIdAsync(id);
+        }
+
+        public async Task<List<Entry>> GetAllAsync()
+        {
+            return await _entryRepository.GetAllAsync();
+        }
+
+        public async Task CreateEntryAsync(Entry entry)
+        {
+            await _entryRepository.AddAsync(entry);
+            await _entryRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ EntryService/Services/IEntryService.cs
@@ -0,0 +1,11 @@
+﻿using EntryService.Entities;
+
+namespace EntryService.Services
+{
+    public interface IEntryService
+    {
+        Task<Entry?> GetByIdAsync(int id);
+        Task<List<Entry>> GetAllAsync();
+        Task CreateEntryAsync(Entry entry)
+    }
+}
