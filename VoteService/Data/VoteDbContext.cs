dfeff02678eebbf3ede1c5fb1ced849c9dc1a6a8
Erdem <erdemsincer123@icloud.com>
2025-05-06 00:47:49 +0300

    vote entity

---
+++ VoteService/Data/VoteDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using VoteService.Entities;
+
+namespace VoteService.Data
+{
+    public class VoteDbContext : DbContext
+    {
+        public VoteDbContext(DbContextOptions<VoteDbContext> options) : base(options) { }
+
+        public DbSet<Vote> Votes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vote>()
+                .HasIndex(v => new { v.EntryId, v.UserId })
+                .IsUnique(); // 👈 Aynı kullanıcı aynı entry'e bir kez oy versin
+        }
+    }
+}
---
+++ VoteService/Entities/Vote.cs
@@ -0,0 +1,13 @@
+﻿namespace VoteService.Entities
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+
+        public int EntryId { get; set; }  // Hangi entry'e oy veriliyor
+        public int UserId { get; set; }   // Oyu veren kullanıcı
+        public bool IsUpvote { get; set; } // true: upvote, false: downvote
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
