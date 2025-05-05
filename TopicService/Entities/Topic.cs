e4fbc5d9f09924c517b9f7effd5236fde93fee41
Erdem <erdemsincer123@icloud.com>
2025-05-05 22:55:23 +0300

    topic entity

---
+++ TopicService/Entities/Topic.cs
@@ -0,0 +1,9 @@
+﻿namespace TopicService.Entities
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
