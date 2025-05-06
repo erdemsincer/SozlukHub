45dc78acb8c7a790feb82c30727e39f86c614f6a
Erdem <erdemsincer123@icloud.com>
2025-05-06 19:00:27 +0300

    review repositories

---
+++ ReviewService/Repositories/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReviewService.Entities;
+
+namespace ReviewService.Repositories
+{
+    public interface IReviewRepository
+    {
+        Task AddAsync(Review review);
+        Task<List<Review>> GetByEntryIdAsync(int entryId);
+        Task<List<Review>> GetByUserIdAsync(int userId);
+        Task SaveChangesAsync();
+    }
+}
---
+++ ReviewService/Repositories/ReviewRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewService.Data;
+using ReviewService.Entities;
+
+namespace ReviewService.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ReviewDbContext _context;
+
+        public ReviewRepository(ReviewDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Review review)
+        {
+            await _context.Reviews.AddAsync(review);
+        }
+
+        public async Task<List<Review>> GetByEntryIdAsync(int entryId)
+        {
+            return await _context.Reviews
+                .Where(r => r.EntryId == entryId)
+                .OrderByDescending(r => r.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<List<Review>> GetByUserIdAsync(int userId)
+        {
+            return await _context.Reviews
+                .Where(r => r.UserId == userId)
+                .OrderByDescending(r => r.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
