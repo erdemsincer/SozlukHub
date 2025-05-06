da819a6ca422e511560aca2c5a86f88f5c398938
Erdem <erdemsincer123@icloud.com>
2025-05-06 19:00:41 +0300

    review dto

---
+++ ReviewService/Dtos/CreateReviewDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewService.Dtos
+{
+    public class CreateReviewDto
+    {
+        public int EntryId { get; set; }
+        public string Content { get; set; } = string.Empty;
+    }
+}
---
+++ ReviewService/Dtos/ReviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ReviewService.Dtos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public int EntryId { get; set; }
+        public string EntryTitle { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
