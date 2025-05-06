128517af20363d12f373a8a85b5bba58c1ccb0bc
Erdem <erdemsincer123@icloud.com>
2025-05-06 16:10:24 +0300

    userresponse dto

---
+++ NotificationService/Models/UserResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationService.Models
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+}
