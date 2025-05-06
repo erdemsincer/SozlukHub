8e775477269c7f8d2429ae5039becf65543a7419
Erdem <erdemsincer123@icloud.com>
2025-05-06 15:33:32 +0300

    notification entity

---
+++ NotificationService/Entities/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace NotificationService.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; } = false;
+    }
+}
