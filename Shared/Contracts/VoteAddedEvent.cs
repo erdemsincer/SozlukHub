fcca5fffcabf5e18f23812cad241ccaa161426cc
Erdem <erdemsincer123@icloud.com>
2025-05-06 15:34:49 +0300

    voteaddeteventt

---
+++ Shared/Contracts/VoteAddedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts
+{
+    public class VoteAddedEvent
+    {
+        public int EntryId { get; set; }
+        public int UserId { get; set; }
+        public bool IsUpvote { get; set; }
+    }
+}
