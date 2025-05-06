b60aa02105ef93cbb7735bc38afe8566b4fe0c92
Erdem <erdemsincer123@icloud.com>
2025-05-07 02:05:02 +0300

    login sayfası

---
+++ sozlukhub-ui/src/pages/LoginPage.jsx
@@ -0,0 +1,62 @@
+﻿import { useState } from "react";
+import axios from "../api/axiosInstance";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("auth/login", {
+                email,
+                password,
+            });
+
+            const token = response.data; // API sadece token döndürüyor
+            localStorage.setItem("token", token);
+
+            alert("✅ Giriş başarılı!");
+            navigate("/");
+        } catch (error) {
+            console.error(error);
+            alert("❌ Giriş başarısız! Email veya şifre yanlış olabilir.");
+        }
+    };
+
+    return (
+        <div style={{ maxWidth: 400, margin: "auto", padding: 20 }}>
+            <h2>Giriş Yap</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="E-posta"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        style={{ width: "100%", marginBottom: 10 }}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Şifre"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        style={{ width: "100%", marginBottom: 10 }}
+                    />
+                </div>
+                <button type="submit" style={{ width: "100%" }}>
+                    Giriş Yap
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
