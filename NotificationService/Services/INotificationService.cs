5947e894bddf83a4d033f074f919454e226c9398
Erdem <erdemsincer123@icloud.com>
2025-05-06 15:33:50 +0300

    notification servies

---
+++ NotificationService/Services/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using NotificationService.Entities;
+
+namespace NotificationService.Services
+{
+    public interface INotificationService
+    {
+        Task<List<Notification>> GetNotificationsAsync(int userId);
+        Task CreateNotificationAsync(int userId, string message);
+        Task MarkAsReadAsync(int id);
+    }
+}
---
+++ NotificationService/Services/NotificationService.cs
@@ -0,0 +1,38 @@
+﻿using NotificationService.Entities;
+using NotificationService.Repositories;
+
+namespace NotificationService.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly INotificationRepository _repository;
+
+        public NotificationService(INotificationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Notification>> GetNotificationsAsync(int userId)
+        {
+            return await _repository.GetAllAsync(userId);
+        }
+
+        public async Task CreateNotificationAsync(int userId, string message)
+        {
+            var notification = new Notification
+            {
+                UserId = userId,
+                Message = message
+            };
+
+            await _repository.AddAsync(notification);
+            await _repository.SaveChangesAsync();
+        }
+
+        public async Task MarkAsReadAsync(int id)
+        {
+            await _repository.MarkAsReadAsync(id);
+            await _repository.SaveChangesAsync();
+        }
+    }
+}
