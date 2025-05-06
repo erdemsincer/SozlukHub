8fca8c9eeb3c48db2a9f8c6e1f17a496ef95bf88
Erdem <erdemsincer123@icloud.com>
2025-05-06 19:00:08 +0300

    review entites

---
+++ ReviewService/Data/ReviewDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewService.Entities;
+using System.Collections.Generic;
+
+namespace ReviewService.Data
+{
+    public class ReviewDbContext : DbContext
+    {
+        public ReviewDbContext(DbContextOptions<ReviewDbContext> options) : base(options) { }
+
+        public DbSet<Review> Reviews => Set<Review>();
+    }
+}
---
+++ ReviewService/Entities/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace ReviewService.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public int EntryId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
