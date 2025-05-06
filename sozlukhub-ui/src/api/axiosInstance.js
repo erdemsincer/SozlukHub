02cfa1b6deaacbd43fcd152dff799fa494c30541
Erdem <erdemsincer123@icloud.com>
2025-05-07 02:05:08 +0300

    api ayarları

---
+++ sozlukhub-ui/src/api/axiosInstance.js
@@ -0,0 +1,21 @@
+﻿import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, // .env'den al
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
